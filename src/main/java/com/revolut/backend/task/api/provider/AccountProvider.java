293f0d3633e83a8611773661e44899eaaafa3fc1
Nikita <Burkov>
2020-02-13 19:33:28 +1000

    Implement speicifc UUID generator for Account id. Account id is generated UUID from number. Improve performance and usability.

---
+++ src/main/java/com/revolut/backend/task/api/provider/AccountProvider.java
@@ -0,0 +1,36 @@
+package com.revolut.backend.task.api.provider;
+
+import com.revolut.backend.task.api.annotation.AccountNumberToUUID;
+
+import javax.ws.rs.ext.ParamConverter;
+import javax.ws.rs.ext.ParamConverterProvider;
+import javax.ws.rs.ext.Provider;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.UUID;
+
+import static java.lang.String.valueOf;
+import static java.util.UUID.nameUUIDFromBytes;
+import static java.util.stream.Stream.of;
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+
+@Provider
+public class AccountProvider implements ParamConverterProvider {
+    @Override
+    public <T> ParamConverter<T> getConverter(Class<T> rawType, Type genericType, Annotation[] annotations) {
+        return of(annotations).noneMatch(a -> a instanceof AccountNumberToUUID) ? null : (ParamConverter<T>) new UuidParamConverter();
+    }
+
+    class UuidParamConverter implements ParamConverter<UUID> {
+
+        @Override
+        public UUID fromString(String value) {
+            return isNumeric(value) ? nameUUIDFromBytes(value.getBytes()) : UUID.fromString(value);
+        }
+
+        @Override
+        public String toString(UUID value) {
+            return valueOf(value);
+        }
+    }
+}
--- src/main/java/com/revolut/backend/task/modules/ApplicationModule.java
+++ src/main/java/com/revolut/backend/task/modules/ApplicationModule.java
@@ -1,13 +1,10 @@
 package com.revolut.backend.task.modules;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Singleton;
-import com.revolut.backend.task.dao.AccountDao;
-import com.revolut.backend.task.dao.impl.AccountDaoImpl;
 
 public class ApplicationModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(AccountDao.class).to(AccountDaoImpl.class).in(Singleton.class);
+
     }
 }
