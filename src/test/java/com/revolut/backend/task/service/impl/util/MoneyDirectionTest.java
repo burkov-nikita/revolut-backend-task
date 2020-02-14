966d48a1a90d495c03060c68fc770e3afa14ff17
Nikita <Burkov>
2020-02-14 12:42:47 +1000

    Add Junit. Implement simple test for Money Direction functions

---
+++ src/test/java/com/revolut/backend/task/service/impl/util/MoneyDirectionTest.java
@@ -0,0 +1,43 @@
+package com.revolut.backend.task.service.impl.util;
+
+import com.revolut.backend.task.entity.Account;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class MoneyDirectionTest {
+
+    @Test
+    public void increaseSaldoTest() {
+        Account account = new Account();
+        BigDecimal amount = new BigDecimal(1000);
+        IntStream.range(0, 1000).forEach(i ->
+                account.changeSaldo(amount, MoneyDirection.INCREASE_SALDO)
+        );
+        assertEquals(new BigDecimal(1000000), account.getSaldo());
+    }
+
+    @Test
+    public void decreaseSaldoTest() {
+        Account account = new Account();
+        BigDecimal amount = new BigDecimal(1000);
+        IntStream.range(0, 1000).forEach(i ->
+                account.changeSaldo(amount, MoneyDirection.DESCREASE_SALDO)
+        );
+        assertEquals(new BigDecimal(-1000000), account.getSaldo());
+    }
+
+    @Test
+    public void mixSaldoTest() {
+        Account account = new Account();
+        BigDecimal amount = new BigDecimal(1000);
+        IntStream.range(0, 1000).forEach(i -> {
+            account.changeSaldo(amount, MoneyDirection.INCREASE_SALDO);
+            account.changeSaldo(amount, MoneyDirection.DESCREASE_SALDO);
+        });
+        assertEquals(new BigDecimal(0), account.getSaldo());
+    }
+}
