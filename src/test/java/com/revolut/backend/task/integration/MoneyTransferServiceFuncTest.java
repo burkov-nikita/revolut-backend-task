574c079ed18137d229547a611da7bd41f1683c9a
Nikita <Burkov>
2020-02-15 15:01:35 +1000

    Implement money transfer test: from-to

--- src/test/java/com/revolut/backend/task/integration/MoneyTransferServiceFuncTest.java
+++ src/test/java/com/revolut/backend/task/integration/MoneyTransferServiceFuncTest.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 import java.util.stream.IntStream;
 
 import static javax.ws.rs.client.Entity.entity;
+import static javax.ws.rs.client.Entity.json;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.junit.Assert.assertEquals;
 
@@ -24,20 +25,25 @@ public class MoneyTransferServiceFuncTest extends JerseyTest {
         return DeploymentContext.builder(ApplicationConfig.class).build();
     }
 
-    @Test
-    public void transferMoneyToFuncTest() {
-        Account rawAccount = new Account(643L, "Test");
+    private Account createAccountAndGet(String metadata) {
+        Account rawAccount = new Account(643L, metadata);
 
-        Account account = target("/account/create")
+        return target("/account/create")
                 .request()
                 .post(entity(rawAccount, APPLICATION_JSON))
                 .readEntity(Account.class);
+    }
+
+
+    @Test
+    public void transferMoneyToFuncTest() {
+        Account account = createAccountAndGet("Test");
 
         IntStream.range(0, 1000).forEach(i -> {
             target("/transfer/to/" + account.getId())
                     .queryParam("amount", AMOUNT.longValue())
                     .request()
-                    .post(Entity.json(null));
+                    .post(json(null));
         });
 
         BigDecimal saldo = target("/account/" + account.getId())
@@ -47,5 +53,59 @@ public class MoneyTransferServiceFuncTest extends JerseyTest {
                 .getSaldo();
 
         assertEquals(saldo, new BigDecimal("100000.00"));
+    }
+
+    @Test
+    public void transferMoneyFromToFuncTest() {
+        Account from = createAccountAndGet("from");
+        Account to = createAccountAndGet("to");
+
+        // Fill 'from' account
+        IntStream.range(0, 1000).forEach(i -> {
+            target("/transfer/to/" + from.getId())
+                    .queryParam("amount", AMOUNT.longValue())
+                    .request()
+                    .post(json(null));
+        });
+
+        IntStream.range(0, 500).forEach(i -> {
+
+            // from-to
+            target("/transfer/from/{from}/to/{to}")
+                    .resolveTemplate("from", from.getId())
+                    .resolveTemplate("to", to.getId())
+                    .queryParam("amount", AMOUNT.longValue())
+                    .request()
+                    .post(json(null));
+
+            // fill from
+            target("/transfer/to/" + from.getId())
+                    .queryParam("amount", AMOUNT.longValue())
+                    .request()
+                    .post(json(null));
+
+            // to-from
+            target("/transfer/from/{from}/to/{to}")
+                    .resolveTemplate("from", to.getId())
+                    .resolveTemplate("to", from.getId())
+                    .queryParam("amount", AMOUNT.longValue())
+                    .request()
+                    .post(json(null));
+        });
+
+        BigDecimal saldoFrom = target("/account/" + from.getId())
+                .request()
+                .get()
+                .readEntity(Account.class)
+                .getSaldo();
+
+        BigDecimal saldoTo = target("/account/" + to.getId())
+                .request()
+                .get()
+                .readEntity(Account.class)
+                .getSaldo();
+
+        assertEquals(saldoFrom, new BigDecimal("150000.00"));
+        assertEquals(saldoTo, new BigDecimal("0.00"));
     }
 }
