d64f7aca2482b065bd75d58455a5c2ff461fc19d
Nikita <Burkov>
2020-02-15 20:42:28 +1000

    Simplicity with Lombok

--- src/main/java/com/revolut/backend/task/service/action/Action.java
+++ src/main/java/com/revolut/backend/task/service/action/Action.java
@@ -2,36 +2,20 @@ package com.revolut.backend.task.service.action;
 
 import com.revolut.backend.task.dto.AccountTransferDTO;
 import com.revolut.backend.task.entity.Account;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.function.Function;
 
 @FunctionalInterface
 public interface Action extends Function<Action.Context, Action.Context> {
 
+    @Getter
+    @Setter
     class Context {
         final private AccountTransferDTO accountTransferDTO;
         private Account creditAccount;
         private Account debitAccount;
-
-        AccountTransferDTO getAccountTransferDTO() {
-            return accountTransferDTO;
-        }
-
-        Account getCreditAccount() {
-            return creditAccount;
-        }
-
-        void setCreditAccount(Account creditAccount) {
-            this.creditAccount = creditAccount;
-        }
-
-        Account getDebitAccount() {
-            return debitAccount;
-        }
-
-        void setDebitAccount(Account debitAccount) {
-            this.debitAccount = debitAccount;
-        }
 
         public Context(AccountTransferDTO accountTransferDTO) {
             this.accountTransferDTO = accountTransferDTO;
