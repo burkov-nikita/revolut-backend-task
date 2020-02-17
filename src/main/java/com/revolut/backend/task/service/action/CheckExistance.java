9c77c050183f3f0475a9f22eeba56ebb4a506901
Nikita <Burkov>
2020-02-17 16:03:58 +1000

    Add Logger for Check existance class. We want know about what accounts are not existed

--- src/main/java/com/revolut/backend/task/service/action/CheckExistance.java
+++ src/main/java/com/revolut/backend/task/service/action/CheckExistance.java
@@ -1,14 +1,23 @@
 package com.revolut.backend.task.service.action;
 
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 import java.util.function.Predicate;
+import java.util.logging.Logger;
 
 @Singleton
 public class CheckExistance implements Predicate<Action.Context> {
 
+    @Inject
+    Logger logger;
+
     @Override
     public boolean test(Action.Context context) {
-        return context.getCreditAccount() != null && context.getDebitAccount() != null;
+        boolean isExist = context.getCreditAccount() != null && context.getDebitAccount() != null;
+        if (!isExist) {
+            logger.warning("Account " + context.getAccountTransferDTO().getCreditAccountNumber() + " or " + context.getAccountTransferDTO().getDebitAccountNumber() + " is not exists");
+        }
+        return isExist;
     }
 }
